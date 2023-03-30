327ef1de3e5bf0a325d56fddfefc500b2b520bb2
SovetovAleksey <91490501+SovetovAleksey@users.noreply.github.com>
2023-03-30 17:08:28 +0300

    Add files via upload

---
+++ metrics.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def recall_at_k(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)[:k]
+
+    flags = np.isin(recommended_list, bought_list)
+
+    recall = flags.sum() / len(bought_list)
+
+    return recall
+
+
+def precision_at_k(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+
+    bought_list = bought_list
+    recommended_list = recommended_list[:k]
+
+    flags = np.isin(bought_list, recommended_list)
+
+    precision = flags.sum() / len(recommended_list)
+
+    return precision
